dccf5577d64bd679e1c8ac19a37dba576d001fe8
asthagaur1 <asthagaur@danfoss.com>
2020-02-01 23:56:02 +0530

    added jenkins file

--- angular7tutorial/src/app/login/login.component.spec.ts
+++ angular7tutorial/src/app/login/login.component.spec.ts
@@ -1,12 +1,19 @@
 import { LoginComponent } from './login.component';
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; 
 
 describe('LoginComponent', () => {
     beforeEach(async(() => {
       TestBed.configureTestingModule({
         imports: [
-          RouterTestingModule
+          RouterTestingModule,
+          BrowserModule,
+          FormsModule,
+          ReactiveFormsModule,
+          HttpClientModule
         ],
         declarations: [
           LoginComponent
